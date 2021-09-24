68891df2a61df16d83c37820a9f77a483e5c4359
Lashonda Miller <lashondamiller007@gmail.com>
2021-09-24 10:06:42 -0400

    new bootcamp lesson added

---
+++ ConsoleApp1Bootcamp2/ConsoleApp1Bootcamp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1Bootcamp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var total = 15500.0; //this also works with var
+            //var days = 65.0;
+            double total = 15500;
+            double days = 65;
+            double PricePerDay = total / days; // made other ways to do it
+            Console.WriteLine($"Bootcamp costs ${total / days} per day");
+            // Console.WriteLine("Bootcamp costs $" + total / days + " per day");
+            //Console.WriteLine($"Bootcamp costs ${PricePerDay} per day");
+        }
+    }
+}
